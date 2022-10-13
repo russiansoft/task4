b24702686439e40d379b00c99ad15ee3c1449938
russiansoft <russiansoft@mail.ru>
2022-10-13 18:01:11 +0300

    + страница по умолчанию

--- public/общие/default.js
+++ public/общие/default.js
@@ -3,8 +3,9 @@ let url = new URL(location);
 let type = decodeURI(url.pathname);
 if (type.startsWith("/"))
 	type = type.substring(1);
+if (!type)
+	type = "задачи";
 document.body.dataset.class = type;
-
 let script = document.createElement("script");
 script.type = "module";
 script.src = type + ".js";
